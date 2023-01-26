ab4c401f206a9b9c0f7867dffb6ca507c4a96a2b
3TH4N <ethanalvarado1999@gmail.com>
2023-01-25 23:23:09 -0500

    Add files via upload

---
+++ groceries.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        item: 'Mac and Cheese',
+        brand: 'Krafts',
+        units: '10 oz',
+        quantity: 3,
+        isPurchased: false
+      }, 
+      {
+        item: 'Ice Cream',
+        brand: 'Breyers',
+        units: '64 oz',
+        quantity: 4,
+        isPurchased: false
+      }, 
+      {
+        item: 'Tortilla',
+        brand: 'Goya',
+        units: '20 oz',
+        quantity: 7,
+        isPurchased: false
+      },
+      {
+        item: 'Hotdogs',
+        brand: 'Nathans',
+        units: '20 oz',
+        quantity: 10,
+        isPurchased: false
+    },
+    {
+        item: 'Cereal',
+        brand: 'Fruity Pebbles',
+        units: '3 oz',
+        quantity: 5,
+        isPurchased: false
+    },
+    {
+        item: 'Soda',
+        brand: 'Fanta',
+        units: '20 oz',
+        quantity: 15,
+        isPurchased: false
+    }
+]
